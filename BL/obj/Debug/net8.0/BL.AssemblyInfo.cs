dd499353466607b1b34055f5c01778d8c4f9b64a
Lenin Ugalde <93264177+Tropicalboy21@users.noreply.github.com>
2024-10-21 19:52:39 -0600

    New directories

--- BL/obj/Debug/net8.0/BL.AssemblyInfo.cs
+++ BL/obj/Debug/net8.0/BL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0661897d1212a6fbd584f228fc5033513c79ccc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10b3eac6294b666fb21ad1a97db68aa98f7b226c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
