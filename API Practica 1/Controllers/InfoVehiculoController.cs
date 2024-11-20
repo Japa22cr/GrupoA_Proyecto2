fecd5e922788d5feff4c4202004e8381adad8764
AndresPMCenfotec <apizarrom@ucenfotec.ac.cr>
2024-11-20 17:56:32 -0600

    InformacionVehiculos

---
+++ API Practica 1/Controllers/InfoVehiculoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DTOs;
+
+
+namespace API_Practica_1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VehiculosController : ControllerBase
+    {
+        private static readonly List<Vehiculo> Vehiculos = new();
+
+        /// <summary>
+        /// Registra un nuevo vehículo.
+        /// </summary>
+        /// <param name="vehiculo">Información del vehículo</param>
+        /// <returns>Vehículo registrado</returns>
+        [HttpPost]
+        public ActionResult<Vehiculo> RegistrarVehiculo([FromBody] Vehiculo vehiculo)
+        {
+            if (vehiculo == null)
+                return BadRequest("La información del vehículo no puede ser nula.");
+
+            Vehiculos.Add(vehiculo);
+            return CreatedAtAction(nameof(ObtenerVehiculo), new { vin = vehiculo.VIN }, vehiculo);
+        }
+
+        /// <summary>
+        /// Obtiene un vehículo por su VIN.
+        /// </summary>
+        /// <param name="vin">Número de VIN</param>
+        /// <returns>Información del vehículo</returns>
+        [HttpGet("{vin}")]
+        public ActionResult<Vehiculo> ObtenerVehiculo(string vin)
+        {
+            var vehiculo = Vehiculos.FirstOrDefault(v => v.VIN == vin);
+            if (vehiculo == null)
+                return NotFound($"El vehículo con VIN {vin} no fue encontrado.");
+
+            return Ok(vehiculo);
+        }
+    }
+}
---
+++ DTOs/InfoVehiculo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs
+{
+    public class Vehiculo
+    {
+        public string VIN { get; set; } 
+        public string NumeroMotor { get; set; } 
+        public int CantidadPuertas { get; set; } 
+        public string Color { get; set; } 
+        public string NumeroPlaca { get; set; } 
+        public string TipoVehiculo { get; set; } 
+    }
+}
