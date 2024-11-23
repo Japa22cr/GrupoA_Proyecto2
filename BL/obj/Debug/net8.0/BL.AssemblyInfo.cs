18dbef6c18b437c9baaa0f2db69bb04b1b0e928e
Kevin Torres <125518994+ktr60@users.noreply.github.com>
2024-11-23 17:23:54 -0600

    nuggets

--- BL/obj/Debug/net8.0/BL.AssemblyInfo.cs
+++ BL/obj/Debug/net8.0/BL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd568866a8282f6be7aa50f981c6442942c1f6ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94519caab3f261630f2a3da898bba86d495768cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
