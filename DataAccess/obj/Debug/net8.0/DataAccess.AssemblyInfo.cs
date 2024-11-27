7725e48d1b9639bdf01817e6ff81daa98c6da1c4
AndresPMCenfotec <apizarrom@ucenfotec.ac.cr>
2024-11-26 21:49:45 -0600

    cache

--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afa968f3c0f2cc6096c3de2369bb7f1c70ebad87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0227122bc58abfa24754beb4f51e4bcc0d6f3870")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
